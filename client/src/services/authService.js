13249f9545431ea34804d8d9c81c9591d8b98f40
Arnav <arnavprajapati3101@gmail.com>
2025-11-11 14:05:07 +0530

    fix deploy issue

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api/";
+const API_URL = "https://budget-expense-tracker-r2jk.onrender.com/api/" || "http://localhost:5000/api/";
 
 const authService = {
     login: async (userData) => {
--- client/src/services/budgetService.js
+++ client/src/services/budgetService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import authService from './authService';
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api/budgets/";
+const API_URL = "https://budget-expense-tracker-r2jk.onrender.com/api/" || "http://localhost:5000/api/budgets/";
 
 const config = () => {
     const token = authService.getToken();
